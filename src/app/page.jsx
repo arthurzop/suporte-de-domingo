a0194518341e387324341179cf31795a58a78429
aarthurzop <medeirosartur48@gmail.com>
2024-06-06 19:32:04 -0300

    testing loader animation

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import Loader from "../components/loader/loader";
+
+export default function Home() {
+
+
+  return (
+    <>
+      <Loader/>
+    </>
+  );
+}
---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,7 @@
+import style from './loader.module.css'
+
+export default function Loader(){
+    return(
+        <div className={style.container}><h1 className={style.title}>Hello World !</h1></div>
+    )
+}
