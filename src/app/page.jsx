d6439e59697d478ed9c267154805e4f5585a28b6
arthurzop <medeirosartur48@gmail.com>
2024-06-06 21:58:35 -0300

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 import styles from "./page.module.css";
-import Loader from "../components/loader/loader";
+import Loader from "../components/loader";
 import HomePage from "../pages/home/home";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
